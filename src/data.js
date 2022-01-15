ac7b8f9fb5eb3b2db7adcb264f61a6e432b76c7a
koya19 <60578460+koya19@users.noreply.github.com>
2022-01-15 16:01:21 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Confédération des Junior-Entreprises Marocaines",
+    subtitle: "CJEM Website",
+    description:
+      "Plateforme présentant la confédération et ses services ainsi que le congrès national",
+    image: "./projet-cjem.png",
+    link: "https://cjem.ma",
+  },
+  {
+    title: "QuinCop",
+    subtitle: "E-commerce website",
+    description:
+      "Quincop.ma est une plateforme marocaine dédiée spécialement aux coopératives",
+    image: "./projet-quincop.png",
+    link: "https://quincop.ma/",
+  },
+  {
+    title: "SarahAirways",
+    subtitle: "Management website",
+    description:
+      "Web Application de gestion de stock et de matériel",
+    image: "./projet-sarahairways.png",
+    link: "https://sarahairways.web.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    name: "Soukaina ISMAILI",
+    company: "NATSU",
+    quote: "Le travail réalisé par ce développeur a été parfait, très bonne communication et s'adapte à tous ce que je demandais.",
+    image: "./client1.png"
+  },
+  {
+    name: "Ali GHAZAL",
+    company: "QuinCop",
+    quote: "Ce talent mérite plus d'admiration, jeune et enthousiaste, il a su prouver aussi beaucoup de professionalisme et de sérieux.",
+    image: "./client2.png"
+  },
+  {
+    name: "Abdelaziz QORAICHI",
+    company: "CJEM",
+    quote: "J'ai eu le plaisir de travailler avec une personne dynamique et très professionnelle dans son travail, il arrive toujours à trouver des solutions aux situations qu'on lui donne.",
+    image: "./client3.png"
+  }
+];
+
+export const skills = ["ReactJs", "django", "WordPress", "HTMLS/CSS", "JavaScript", "FireBase"];
